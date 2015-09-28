a6c009e25779cdc8d2636aa514d1fcff7742be82
Jorge <jorgecastanedagarcia@gmail.com>
2015-09-28 12:06:29 -0500

    Terminado }Beta

---
+++ executable.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd /home/jorge/Documentos/
+continuar=y
+while [ $continuar = y ]:
+do
+{
+
+echo 'Bash para crear una "escuela" con los siguentes grupos:'
+echo 'Administrativos, Profesores y Alumnos'
+echo '¿Qué proceso desea hacer?'
+echo '1.- Borrar todos los directorios y grupos'
+echo '2.- Crear todos los grupos y directorios de los grupos'
+echo '3.- Crear usuarios: alumnos, profesores, administrativos'
+read opc
+if [ $opc = 1 ]; then
+	sudo sh borrar.sh
+	echo '¿Desea continuar?,presione [y] si o [n] para salir'
+	read opt
+	continuar = opt
+fi
+if [ $opc = 1 ]; then
+	sudo sh carpetas.sh
+	echo '¿Desea continuar?,presione [y] si o [n] para salir'
+	read opt
+	continuar = opt
+fi
+if [ $opc = 1 ]; then
+	sudo sh escuela.sh
+	echo '¿Desea continuar?,presione [y] si o [n] para salir'
+	read opt
+	continuar = opt
+fi
+}
+done
